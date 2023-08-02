fff68be4a607b9c550667938c384edf77dda1bca
Theadek <theadek10@gmail.com>
2023-08-02 16:31:32 +0200

    Polish Game no. 1
    Add CountdownUI number pop-up
    Add TutorialUI
    Add Player walking particles
    Add Walls and game scene background

--- Assets/Scripts/UI/GameStartCountdownUI.cs
+++ Assets/Scripts/UI/GameStartCountdownUI.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 
 public class GameStartCountdownUI : MonoBehaviour
 {
+    private const string NUMBER_POPUP = "NumberPopup";
+
     [SerializeField] private TextMeshProUGUI countdownText;
+
+    private Animator animator;
+    private int previousCountdownNumber;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void Start()
     {
         KitchenGameManager.Instance.OnStateChanged += KitchenGameManager_OnStateChanged;
+        Hide();
     }
 
     private void KitchenGameManager_OnStateChanged(object sender, System.EventArgs e)
@@ -26,7 +37,15 @@ public class GameStartCountdownUI : MonoBehaviour
 
     private void Update()
     {
-        countdownText.text = Mathf.Ceil(KitchenGameManager.Instance.GetCountdownToStartTimer()).ToString();
+        int countdownNumber = Mathf.CeilToInt(KitchenGameManager.Instance.GetCountdownToStartTimer());
+        countdownText.text = countdownNumber.ToString();
+
+        if(previousCountdownNumber != countdownNumber)
+        {
+            previousCountdownNumber = countdownNumber;
+            animator.SetTrigger(NUMBER_POPUP);
+            SoundManager.Instance.PlayCountdownSound();
+        }
     }
 
     private void Show()
